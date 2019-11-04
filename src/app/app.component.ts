0b6c8fb38cb48d29d00f8b4e83c51d736e975da6
Alejandro Asensio <alejandro.asensio.10@gmail.com>
2019-11-04 17:07:29 +0100

    changed favicon, refactored email to FormControl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core'
-// import { Md5 } from 'ts-md5/dist/md5'
-import { AppService } from './app.service'
+import { FormControl } from '@angular/forms'
 import { Annotator, Link } from './app.models'
-import { FormBuilder, FormGroup } from '@angular/forms'
+import { AppService } from './app.service'
 
 
 @Component({
@@ -13,47 +12,19 @@ import { FormBuilder, FormGroup } from '@angular/forms'
 export class AppComponent implements OnInit {
 
   annotator: Annotator
-  emailForm: FormGroup
+  email = new FormControl()
   links: Link[]
 
-  constructor(private appService: AppService, private formBuilder: FormBuilder) {
-    this.emailForm = this.formBuilder.group({
-      email: ''
-    })
-  }
+  constructor(private appService: AppService) { }
 
   ngOnInit() {
-    this.getLinks()
-    // this.generateHashes()
-  }
-
-  getLinks() {
     this.appService.getLinks().subscribe(data => this.links = data)
   }
 
   loadArticles(email: string) {
     this.appService.getAnnotators().subscribe(
-      data => this.annotator = data.find(annotator => annotator.email === email)
+      data => this.annotator = data.find(annotator => annotator.email === this.email.value)
     )
   }
-
-  // generateHashes() {
-  //   this.appService.getSamples().subscribe(
-  //     data => {
-  //       const updatedCompanies: Annotator[] = []
-  //       data.forEach(
-  //         sample => {
-  //           const updatedAnnotator: Annotator = {
-  //             id: sample.annotator,
-  //             hash: Md5.hashStr('sample.company'),
-  //             articles: sample.articles
-  //           }
-  //           updatedCompanies.push(updatedAnnotator)
-  //         }
-  //       )
-  //       console.log(JSON.stringify(updatedCompanies))
-  //     }
-  //   )
-  // }
 
 }
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -2,16 +2,14 @@ import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs'
 import { Annotator, Link } from './app.models'
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
 
   constructor(private http: HttpClient) { }
-
-  // getSamples(): Observable<any[]> {
-  //   return this.http.get<any[]>('assets/samples.json')
-  // }
 
   getAnnotators(): Observable<Annotator[]> {
     return this.http.get<Annotator[]>('assets/samples.json')
@@ -20,4 +18,5 @@ export class AppService {
   getLinks(): Observable<Link[]> {
     return this.http.get<Link[]>('assets/links.json')
   }
+
 }
